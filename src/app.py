22adcd5ae22bc4aaa1ce9e195bf5f18c7515e8ff
Bartek Kryza <bkryza@gmail.com>
2024-09-24 13:08:10 +0200

    VFS-12196 Added host and port command line options

--- src/app.py
+++ src/app.py
@@ -2,14 +2,20 @@
 
 import connexion
 import logging
-
-import sys
-import os
+import argparse
 
 app = connexion.App(__name__, specification_dir='./swagger/')
 app.add_api('swagger.yaml', strict_validation=True)
 
 logging.basicConfig(level=logging.INFO)
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+            description='Onedata Handle Proxy service implementation for PID handles.')
+    parser.add_argument('--host', type=str, default='0.0.0.0', help='Hostname to bind to. Default is 0.0.0.0')
+    parser.add_argument('--port', type=int, default=8080, help='Port to bind to. Default is 8080')
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    app.run(port=8080)
+    args = parse_args()
+    app.run(host=args.host, port=args.port)
